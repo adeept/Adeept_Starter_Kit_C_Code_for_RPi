5054a6bd804eef65644076799df0d7c50a257d86
Jason <adeept@163.com>
2015-05-02 09:11:02 -0700

    add a header comment

--- 06_breathingLed/06_breathingLed.c
+++ 06_breathingLed/06_breathingLed.c
@@ -1,3 +1,11 @@
+/*
+* File name   : 06_breathingLed.c
+* Description : ...
+* Website     : www.adeept.com
+* E-mail      : support@adeept.com
+* Author      : Jason
+* Date        : 2015/05/02
+*/
 #include <wiringPi.h>
 #include <stdio.h>
 
