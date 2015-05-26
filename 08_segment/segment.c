23533362d5c38b19527d50567cb51c0849c7c6e4
adeept <adeept@163.com>
2015-05-26 09:12:34 +0000

    add some code

--- 08_segment/segment.c
+++ 08_segment/segment.c
@@ -1,6 +1,6 @@
 /*
 * File name   : segment.c
-* Description : .
+* Description : display 0~9, A~F on 7-segment display
 * Website     : www.adeept.com
 * E-mail      : support@adeept.com
 * Author      : Jason
@@ -11,10 +11,29 @@
 
 typedef unsigned char uchar;
 
-unsigned char SegCode[17] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x80};
+const uchar SegCode[17] = {0x3f,0x06,0x5b,0x4f,0x66,0x6d,0x7d,0x07,0x7f,0x6f,0x77,0x7c,0x39,0x5e,0x79,0x71,0x80};
 
 int main(void)
 {
+	int i;
+
+	if(wiringPiSetup() < 0){ // setup wiringPi
+		printf("wiringPi setup failed !\n");
+		return -1;
+	}
+
+	for(i = 0; i < 8; i++){  // set pin mode as output(GPIO0~GPIO7)
+		pinMode(i, OUTPUT);
+	}
+
+	while(1){
+		for(i = 0; i < sizeof(SegCode)/sizeof(uchar); i++){ // display 0~9,A~F
+			digitalWriteByte(SegCode[i]);
+			delay(500);
+		}
+		digitalWriteByte(0x00);   //segment off
+		delay(1000);
+	}
 
 	return 0;
 }
